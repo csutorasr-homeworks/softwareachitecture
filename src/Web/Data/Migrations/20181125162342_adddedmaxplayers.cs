3b0bdf2bc62c77dd7ab61c0bf48993261ac37e3a
Lajos Zongor <zongorla@gmail.com>
2018-11-25 17:24:08 +0100

    asddafdsfg

---
+++ src/Web/Data/Migrations/20181125162342_adddedmaxplayers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Data.Migrations
+{
+    public partial class adddedmaxplayers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxUsers",
+                table: "GameSessions",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxUsers",
+                table: "GameSessions");
+        }
+    }
+}
