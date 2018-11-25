8f5264c9faab83a0ab9e4b29f55ae4ef8d706f65
Csutorás Robin <csutorasr@gmail.com>
2018-11-25 22:51:29 +0100

    feat: userid is on the view

--- src/Web/Pages/Play.cshtml.cs
+++ src/Web/Pages/Play.cshtml.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace Web.Pages
 {
     [Authorize]
     public class PlayModel : PageModel
     {
+        public string UserId { get; private set; }
+
         public void OnGet()
         {
+            UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
         }
     }
 }
