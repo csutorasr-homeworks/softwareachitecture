2a024dd864baa6fc8452d5266f516b4c8c240adf
Lajos Zongor <zongorla@gmail.com>
2018-11-25 23:33:19 +0100

    dfdafsdaf

--- src/Web/Hubs/GameHub.cs
+++ src/Web/Hubs/GameHub.cs
@@ -96,6 +96,7 @@ namespace Web.Hubs
                 gameSession.WaitingForPlayers = false;
                 gameSession.InProgress = true;
                 gameSession = await gameRepository.UpdateGame(gameSession);
+                await GetGames();
                 await Clients.Group(gameSession.Id.ToString()).SendAsync("PlayerConnected", new PlayerConnectedViewModel(gameSession));
                 var question = await gameRepository.GetQuestion(gameSession.Id);
                 await Clients.Group(gameSession.Id.ToString()).SendAsync("QuestionRecieved", new QuestionViewModel(question));
